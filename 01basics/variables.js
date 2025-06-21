b6b0fb20a8c36601e06e8a47c3914f6d0a8d6e85
Ritesh kumar <122215509+Ritesh983@users.noreply.github.com>
2025-06-21 13:01:26 +0000

    variables added

---
+++ 01basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=144553 
+let accountMail="ritesh@1234"
+var accountPassword="12345"
+accountCity="Patna"
+
+//accountId=2  not allowed 
+acccountMail="rk@gmail.com"
+accountPassword="212123"
+accountCity="Patliputra"
+
+console.log(accountId);
+
+console.table([accountId,accountMail,accountPassword,accountCity])
