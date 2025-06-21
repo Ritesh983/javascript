700e83284d95b99c1476133f22e5df2862f63ccf
Ritesh kumar <122215509+Ritesh983@users.noreply.github.com>
2025-06-21 13:50:28 +0000

    typecasting

---
+++ 01basics/typecast.js
@@ -0,0 +1,27 @@
+let score="33"
+
+console.log(typeof(score));
+
+let valueInNumber= Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber))
+
+let score2="33abc"
+console.log(score2);
+console.log(typeof score2);
+let valueInNumber2=Number(score2);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
+
+
+
+
+
